be395377eefe070d110de998b351796a60019ac3
mohachi <hachiassali15@gmail.com>
2024-08-17 17:22:54 +0100

    test: fix test

--- tests/usage.php
+++ tests/usage.php
@@ -1,15 +1,13 @@
 <?php
 
 use Mohachi\Xwoole\Http\Server;
-use OpenSwoole\Core\Psr\Request;
 use OpenSwoole\Core\Psr\Response;
-use OpenSwoole\Core\Psr\Stream;
 
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 $server = new Server("localhost", 1111);
 
-$server->get("/", function(Request $request)
+$server->get("/", function()
 {
     return new Response("welcome\n");
 });
